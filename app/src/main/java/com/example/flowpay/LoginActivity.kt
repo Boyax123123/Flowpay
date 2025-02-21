10e469a5feae09f7b862da46b88a6c6f048ad6bf
Yobs <josemarpajares17@gmail.com>
2025-02-21 09:09:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/flowpay/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.flowpay
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        val button_register: Button = findViewById<Button>(R.id.button_register )
+
+        button_register.setOnClickListener {
+            Log.e("CSIT 284", "Button is clicked")
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
